f232f4d0fd59b1917a6d5ab9ee5faae9f7bda070
Nida Yucedal <yucedalnida@gmail.com>
2023-02-05 18:39:46 +0300

    read&writefile

--- cypress/e2e/day07/03_write_and_read_file.cy.js
+++ cypress/e2e/day07/03_write_and_read_file.cy.js
@@ -2,13 +2,15 @@
 
 describe('Write&Read File',() => {
 
-    it('', () => {
+    it('Wrire&Read File with Json', () => {
        
-   
-    })
-   
-   
+        cy.writeFile('./cypress/e2e/day07/UserData.json', { name: 'Eliza', email: 'eliza@example.com'})
 
-   })
+        cy.readFile('./cypress/e2e/day07/UserData.json').then( (user) => {
+            expect(user.name).to.equal('Eliza')
+            expect(user.email).to.equal('eliza@example.com')
+        })
+    })
+})
    
    
