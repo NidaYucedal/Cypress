e62d51fc6ddcaab158b8c2bd270260386b302413
Nida Yucedal <yucedalnida@gmail.com>
2023-02-25 12:33:22 +0300

    json

---
+++ cypress/e2e/day09/01_JSON_Object.cy.js
@@ -0,0 +1,46 @@
+/// <reference types="cypress" />
+
+describe('JSON Object', () => {
+
+    it('JSON Object', () => {
+
+        cy.visit('https://www.google.com')
+
+        const simpleObject = {"key1": "value1", "key2": "value2", "key3": "value3"}
+        console.log(simpleObject.key2)//value2
+        console.log(simpleObject["key3"])//value3
+
+        const simpleArrayOfValue = ["one", "two", "three"]
+        console.log(simpleArrayOfValue[0])//one
+
+        const arrayObject = [{ "key1": "value1"}, {"key2": "value2"}, {"key3": "value3"}]
+        console.log(arrayObject[2].key3)//value3
+
+        const dataType = {"string": "metin", "number": 15}
+
+        const body = {"Student" : [{"FirstName" : "Sena","LastName" : "Cengiz"},
+                                     {"FirstName" : "Yusuf","LastName" : "Aslan"},
+                                     {"FirstName" : "Derya","LastName" : "Soylu"}]}
+        console.log(body.Student[2].LastName)
+        console.log(body.Student[1].FirstName)
+        console.log(body)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+        
+    })
+})
