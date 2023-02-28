0e5daa888f1fdfa4fdc7c120fd536f65aab36a05
Nida Yucedal <yucedalnida@gmail.com>
2023-02-28 11:48:05 +0300

    api

---
+++ cypress/e2e/day09/02_simple_api_test.cy.js
@@ -0,0 +1,27 @@
+//https://jsonplaceholder.cypress.io/comments
+
+/// <reference types="cypress" />
+
+describe('Simple API Test', () => {
+    const requestURL = 'https://jsonplaceholder.cypress.io/comments'
+
+    it('Header Assert 1', () => {
+        cy.request(requestURL).its('headers').its('content-type')
+        .should('include', 'application/json; charset=utf-8')
+    })
+
+    it('Header Assert2', () => {
+        cy.request({
+            url: requestURL,
+            //url : 'https://jsonplaceholder.cypress.io/comments'
+            method: 'GET'})
+            .its('headers').its('content-type')
+        .should('include', 'application/json; charset=utf-8')
+    })
+
+
+
+
+
+    
+})
