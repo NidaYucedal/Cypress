d259781ef53589e0ff77dc1753c3bafe05bce4c9
Nida Yucedal <yucedalnida@gmail.com>
2023-03-06 10:01:21 +0300

    practise

--- cypress/e2e/day08/02_practise.cy.js
+++ cypress/e2e/day08/02_practise.cy.js
@@ -5,8 +5,8 @@ describe ('', () => {
     it ('', () => {
 
         cy.visit('https://www.google.com');
-        const searchbox=cy.get("//input[@name='q']");
-        searchbox.type('amazon');
+        const searchbox=cy.get("input[name='q']");
+        searchbox.type('amazon').click();
 
 
     })
